d5e7113ebe4f0f88fc2d085e2880b193a36db2d5
Hendri Juhanda <hendri.juhanda@gmail.com>
2024-05-29 12:06:47 +0700

    feat: setup react spa

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/{fe_capture?}', function () {
+    return view('app');
+})->where('fe_capture', '^(?!api).*$');
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import react from "@vitejs/plugin-react";
+import laravel from "laravel-vite-plugin";
+import { fileURLToPath } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+
+      laravel({
+        input: ["resources/css/app.css", "resources/js/app.js"],
+        refresh: true,
+      }),
+    ],
+
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: fileURLToPath(
+            new URL("./resources/react", import.meta.url)
+          ),
+        },
+      ],
+    },
+
+    server: {
+      host: "0.0.0.0",
+      port: Number(env.VITE_HMR_PORT),
+      hmr: {
+        host: "localhost",
+      },
+    },
+  };
+});
